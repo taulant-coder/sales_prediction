65855d2efbf68b3bad1957a94ce97586efb76aa8
taulant-coder <70835261+taulant-coder@users.noreply.github.com>
2022-06-02 12:39:21 +0200

    Project upload

---
+++ sales.R
@@ -0,0 +1,34 @@
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Simple Linear Regression to the Training set
+regresor = lm(formula = Salary ~ YearsExperience,
+              data = training_set)
+# Predicting the Test set results
+y_pred = predict(regresor, newdata = test_set)
+
+# Visualising the Training set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regresor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Training set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
+
